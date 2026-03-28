9e76ef6f35a888916b9d301727270c34d6c9b55d
ChiushihAn <18710496906@163.com>
2026-03-28 14:53:46 +0800

    ex06:进一步完善相关注释和代码

--- ex06/ex06.ino
+++ ex06/ex06.ino
@@ -4,7 +4,7 @@ const int ledPinY = 4;   // 黄灯
 
 // PWM 配置
 const int pwmFreq = 5000;       // 频率 5kHz
-const int pwmResolution = 8;    // 分辨率 8 位（0~255）
+const int pwmResolution = 8;    // 分辨率 8 位
 
 void setup() {
   // 初始化两个 PWM 通道（ledcAttach 会自动分配可用通道）
@@ -24,10 +24,10 @@ void loop() {
     delay(10);                          // 控制渐变速度（10ms 过渡平滑）
   }
   
-  // 阶段2：红灯逐渐变暗，黄灯 逐渐变亮
+  // 阶段2：红灯逐渐变暗，黄灯逐渐变亮
   for (int duty = 255; duty >= 0; duty--) {
-    ledcWrite(ledPinR, duty);           // 红灯 亮度减少
-    ledcWrite(ledPinY, 255 - duty);     //黄灯 亮度增加
+    ledcWrite(ledPinR, duty);           // 红灯亮度减少
+    ledcWrite(ledPinY, 255 - duty);     //黄灯亮度增加
     delay(10);
   }
 }
